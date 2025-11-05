2757f898b45fcf2a109e63015bf344820bfe69fd
Michael Artru <38781991+Steezycoding@users.noreply.github.com>
2025-11-05 12:07:10 +0100

    [Gateway] Initialisation du microservice
    * :tada: Project initialization
    
    * :sparkles: Created 1st secured route

---
+++ gateway/src/main/java/com/medilabo/gateway/config/security/SecurityFilterChainConfig.java
@@ -0,0 +1,25 @@
+package com.medilabo.gateway.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilterChainConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/actuator/health").permitAll()
+						.anyRequest().authenticated()
+				)
+				.httpBasic(Customizer.withDefaults())
+				.build();
+	}
+}
