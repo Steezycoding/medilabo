33a3fc6dd1a35a1f24038b75defa53e09dc71621
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-11-16 19:28:18 +0100

    :sparkles: Implement AuthInterceptor for HTTP requests to include authorization token

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,13 +2,14 @@ import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChange
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {AuthInterceptor} from './security/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptors([AuthInterceptor]))
   ]
 };
