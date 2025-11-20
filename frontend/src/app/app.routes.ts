5bcf45fd87e9ad78620972dc2c971c45d911c855
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-11-20 12:52:23 +0100

    :sparkles: Add patient details component and his route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {authGuard} from './security/auth.guard';
 import {HomeComponent} from './components/home/home';
 import {DashboardComponent} from './components/dashboard/dashboard';
 import {LoginFormComponent} from './components/login/login';
+import {PatientDetailsComponent} from './components/patient/patient-details/patient-details';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'login', component: LoginFormComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'patient/:id', component: PatientDetailsComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
 ];
---
+++ frontend/src/app/components/patient/patient-details/patient-details.ts
@@ -0,0 +1,37 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Patient} from '../../../model/Patient';
+import {PatientService} from '../../../services/patient.service';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-patient-details',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './patient-details.html',
+  styleUrl: './patient-details.scss',
+})
+export class PatientDetailsComponent implements OnInit {
+  protected patient: Patient = null!;
+  patientId: string = '';
+
+  route = inject(ActivatedRoute);
+  patientService = inject(PatientService);
+
+  ngOnInit(): void {
+    this.patientId = this.route.snapshot.paramMap.get('id') || '';
+    this.patientService.getPatientById(this.patientId).subscribe({
+      next: data => {
+        this.patient = data;
+      },
+      error: err => {
+        console.error("Error fetching patient data")
+      }
+    });
+  }
+
+  onSubmitPatientForm() {
+    console.log("Patient form submitted:", this.patient);
+  }
+}
