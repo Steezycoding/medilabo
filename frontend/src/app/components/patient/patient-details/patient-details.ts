4028b53d7f4ba6f4287f58dace5e21ed8d9c9e98
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-11-26 14:50:37 +0100

    :lipstick: Enhanced patient form to handle update and create patient

--- frontend/src/app/components/patient/patient-details/patient-details.ts
+++ frontend/src/app/components/patient/patient-details/patient-details.ts
@@ -2,7 +2,7 @@ import {Component, inject, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Patient} from '../../../model/Patient';
 import {PatientService} from '../../../services/patient.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-patient-details',
@@ -13,31 +13,70 @@ import {ActivatedRoute} from '@angular/router';
   styleUrl: './patient-details.scss',
 })
 export class PatientDetailsComponent implements OnInit {
-  patient: Patient = null!;
+  patient: Patient = {} as Patient;
   patientId: string = '';
+  isEditMode = false;
+  errorFetching = false;
+  errorMessage: string = '';
+  loading = false;
+  fallbackRoute: string = '/dashboard';
 
-  route = inject(ActivatedRoute);
-  patientService = inject(PatientService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private patientService = inject(PatientService);
 
   ngOnInit(): void {
     this.patientId = this.route.snapshot.paramMap.get('id') || '';
+    this.isEditMode = this.patientId !== '';
 
-    if (this.patientId === '') {
-      console.error('Patient ID is missing in route parameters');
-      return;
-    }
-
-    this.patientService.getPatientById(this.patientId).subscribe({
-      next: data => {
-        this.patient = data;
-      },
-      error: err => {
-        console.error(err)
+    if (this.isEditMode) {
+      if (this.patientId === '') {
+        console.error('Patient ID is missing in route parameters');
+        return;
       }
-    });
+
+      this.loading = true;
+      this.patientService.getPatientById(this.patientId).subscribe({
+        next: data => {
+          this.patient = data;
+          this.loading = false;
+        },
+        error: err => {
+          this.errorFetching = true;
+          this.errorMessage = `The patient with ID ${this.patientId} could not be found.`;
+          console.error(err)
+          this.loading = false;
+        }
+      });
+    } else {
+      this.patient = {} as Patient;
+    }
   }
 
   onSubmitPatientForm() {
-    this.patientService.updatePatient(this.patientId, this.patient);
+    this.loading = true;
+    if (this.isEditMode) {
+      this.patientService.updatePatient(this.patientId, this.patient).subscribe({
+        next: (): void => {
+          this.loading = false;
+          this.router.navigate([this.fallbackRoute]);
+        },
+        error: (err: any) => {
+          console.error('Patient update failed.', err);
+          this.loading = false;
+        }
+      });
+    } else {
+      this.patientService.createPatient(this.patient).subscribe({
+        next: (): void => {
+          this.loading = false;
+          this.router.navigate([this.fallbackRoute]);
+        },
+        error: (err: any) => {
+          console.error('Patient creation failed.', err);
+          this.loading = false;
+        }
+      });
+    }
   }
 }
