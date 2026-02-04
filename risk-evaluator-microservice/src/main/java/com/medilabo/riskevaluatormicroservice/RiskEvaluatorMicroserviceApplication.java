90f2f5b10fec58800261e1311ad197bf645ea16b
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2026-02-04 16:33:21 +0100

    :tada: Initialize risk evaluator microservice

---
+++ risk-evaluator-microservice/src/main/java/com/medilabo/riskevaluatormicroservice/RiskEvaluatorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.riskevaluatormicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskEvaluatorMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskEvaluatorMicroserviceApplication.class, args);
+	}
+
+}
