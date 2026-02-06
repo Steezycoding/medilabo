16b1af81d2092e9d7fa24cd5934c48d6f6030b08
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2026-02-06 14:19:23 +0100

    :sparkles: [MedicalNote-MS] Add development-specific security filter chain configuration

--- medical-note-microservice/src/main/java/com/medilabo/medicalnotemicroservice/config/security/SecurityFilterChainConfig.java
+++ medical-note-microservice/src/main/java/com/medilabo/medicalnotemicroservice/config/security/SecurityFilterChainConfig.java
@@ -2,6 +2,7 @@ package com.medilabo.medicalnotemicroservice.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,6 +11,17 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityFilterChainConfig {
 	@Bean
+	@Profile("dev")
+	SecurityFilterChain securityFilterChainDev(HttpSecurity http) throws Exception {
+		return http
+				.csrf(csrf -> csrf.disable())
+				.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				.build();
+	}
+
+	@Bean
+	@Profile("!dev")
 	SecurityFilterChain securityFilterChain(HttpSecurity http, JwtVerifierService jwtVerifierService) throws Exception {
 		return http
 				.csrf(csrf -> csrf.disable())
@@ -22,6 +34,7 @@ public class SecurityFilterChainConfig {
 	}
 
 	@Bean
+	@Profile("!dev")
 	GatewayJwtFilter gatewayJwtFilter(JwtVerifierService jwtVerifierService) {
 		return new GatewayJwtFilter(jwtVerifierService);
 	}
