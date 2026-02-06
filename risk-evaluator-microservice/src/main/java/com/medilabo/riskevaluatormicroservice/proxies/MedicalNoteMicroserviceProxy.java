b2a18687f633da16b48f3303341ddfc01e20e3d1
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2026-02-06 14:19:52 +0100

    :wrench: Implemented Medical Note microservice integration and configuration

---
+++ risk-evaluator-microservice/src/main/java/com/medilabo/riskevaluatormicroservice/proxies/MedicalNoteMicroserviceProxy.java
@@ -0,0 +1,15 @@
+package com.medilabo.riskevaluatormicroservice.proxies;
+
+import com.medilabo.riskevaluatormicroservice.beans.MedicalNoteBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "medical-note-microservice")
+public interface MedicalNoteMicroserviceProxy {
+
+	@GetMapping("medical-notes/patient/{id}")
+	List<MedicalNoteBean> getPatientMedicalNotes(@PathVariable("id") long id);
+}
