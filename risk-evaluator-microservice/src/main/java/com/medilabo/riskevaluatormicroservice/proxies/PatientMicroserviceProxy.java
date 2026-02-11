fe2c95be653bf2837a0b509c8d73a0aa95874fce
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2026-02-11 15:37:22 +0100

    :wrench: Added Feign client configuration and interceptor for JWT token management

--- risk-evaluator-microservice/src/main/java/com/medilabo/riskevaluatormicroservice/proxies/PatientMicroserviceProxy.java
+++ risk-evaluator-microservice/src/main/java/com/medilabo/riskevaluatormicroservice/proxies/PatientMicroserviceProxy.java
@@ -1,11 +1,12 @@
 package com.medilabo.riskevaluatormicroservice.proxies;
 
 import com.medilabo.riskevaluatormicroservice.beans.PatientBean;
+import com.medilabo.riskevaluatormicroservice.config.feign.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "patient-microservice")
+@FeignClient(name = "patient-microservice", configuration = FeignClientConfig.class)
 public interface PatientMicroserviceProxy {
 
 	@GetMapping("/patients/{id}")
